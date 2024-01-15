5de48469db229d146c614d8b9ab8281adcea75cf
Gusatvo Ribeiro <gustavoribe00@outlook.com>
2024-01-15 09:35:19 -0300

    Exercício python: Dobro, triplo e raiz quadrada

---
+++ EX006.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um numero: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+
+print(f'O numero digitado foi {n}, o dobro de {n} é {d}, o triplo de {n} é {t}, e a raiz quadrada de {n} é {r}')
