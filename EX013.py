880aa080ba5a057b2b2937f0cc230ac7627faa1d
Gusatvo Ribeiro <gustavoribe00@outlook.com>
2024-01-19 06:36:39 -0300

    Exercício python:  Reajuste Salarial

---
+++ EX013.py
@@ -0,0 +1,5 @@
+salario = float(input('Digite o salário do funcionário: R$'))
+
+aumento = salario + ((salario * 15) / 100)
+
+print(f'O funcionário que antes ganhava {salario:.2f}, agora com 15% de aumento recebera um salário de {aumento:.2f}')
